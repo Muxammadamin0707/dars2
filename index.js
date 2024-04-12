b5ba002f6b3ba2b16b5ad47697563f4a4425da5a
Muxammadamin Zoirov <muxammadaminzoirov@gmail.com>
2024-04-12 11:49:12 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,9 @@
+let matn = String(prompt("String turdagi ma'lumot kriting: "));
+let son = Number(prompt("Number turdagi ma'lumot kriting: "));
+let rost = Boolean(prompt("Boolean turdagi ma'lumot kriting: "));
+
+confirm("Hamma ma'lumolar to'g'rima?");
+
+alert("Siznng kiritgan " + matn + " so'zi " + typeof matn + " typega tegishli!");
+alert("Siznng kiritgan " + son + " soni " + typeof son + " typega tegishli!");
+alert("Siznng kiritgan " + rost + " so'zi " + typeof rost + " typega tegishli!");
